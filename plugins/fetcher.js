0f0b3d33c5cee1b5e8e62b9f9c5ad09ee37217a0
Jon Lyttle <jon@jonlyttle.com>
2025-07-04 11:16:21 -0500

    Parse headers from protoweb when fetching pages

--- plugins/fetcher.js
+++ plugins/fetcher.js
@@ -1,33 +1,48 @@
 const fp = require('fastify-plugin');
 const { execa } = require('execa');
 
-//TODO: We will want to replace this with something like undici
-//Protoweb doesn't send HTTP/1.0 compliant headers (missing CR)
-//Until this is addressed, we need to use something lenient like curl
+// Helper to parse curl -i output into { statusCode, headers, body }
+function parseCurlResponse(output, defaultStatus = 200) {
+  const headerEnd = output.indexOf('\n\n');
+  const rawHeaders = output.slice(0, headerEnd);
+  const body = output.slice(headerEnd);
+  const headerLines = rawHeaders.split(/\r?\n/);
+  const statusLine = headerLines.shift();
+  const statusMatch = statusLine && statusLine.match(/HTTP\/\d+\.\d+\s+(\d+)/);
+  const statusCode = statusMatch ? parseInt(statusMatch[1], 10) : defaultStatus;
+  const headers = {};
+  for (const line of headerLines) {
+    const idx = line.indexOf(':');
+    if (idx !== -1) {
+      const key = line.slice(0, idx).trim().toLowerCase();
+      const value = line.slice(idx + 1).trim();
+      headers[key] = value;
+    }
+  }
+  return { statusCode, headers, body };
+}
+
 async function fetcherPlugin(fastify, opts) {
   fastify.decorate('fetchRemote', async (url, options = {}) => {
     try {
+      // Use -i to include headers, and do NOT use --location so redirects are not followed
       const { stdout } = await execa('curl', [
         '--silent',
-        '--location',
+        '-i',
         '--proxy', 'http://wayback.protoweb.org:7851',
         url
       ]);
 
-      return {
-        statusCode: 200,
-        headers: { 'content-type': 'text/html' },
-        body: stdout,
-      };
+      const { statusCode, headers, body } = parseCurlResponse(stdout, 200);
+      fastify.log.info('[fetchRemote] Parsed headers:', headers);
+      fastify.log.info('[fetchRemote] Parsed statusCode:', statusCode);
+      return { statusCode, headers, body };
     } catch (err) {
       if (err.exitCode === 18 && err.stdout) {
         // treat it as a soft failure: return partial response
         fastify.log.warn(`Partial file from curl, using partial data for ${url}`);
-        return {
-          statusCode: 206,
-          headers: { 'content-type': 'text/html' },
-          body: err.stdout,
-        };
+        const { statusCode, headers, body } = parseCurlResponse(err.stdout, 206);
+        return { statusCode, headers, body };
       }
       fastify.log.error(`Error fetching via curl: ${url}`);
       throw err;
